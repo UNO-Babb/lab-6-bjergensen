d4f0f5e2be561d03fe834c2a40968fb126406675
bjergensen <bjergensen@unomaha.edu>
2025-03-03 02:22:41 +0000

    done

--- DiceRoll.py
+++ DiceRoll.py
@@ -7,12 +7,26 @@ import random
 def main():
   #Create an empty list with possible roll values
   rolls = [0,0,0,0,0,0,0,0,0,0,0,0]
+  sum_rolls = [0] * 11
   #Create two dice values ranging from 1 - 6 each
-  
-  #find the sum total of the two dice
-  
-  #print statictics for dice rolls
+  for r in range(10000):
+    dice1 = random.randint(1,6)
+    rolls[dice1 - 1] = rolls[dice1 - 1] + 1
+    dice2 = random.randint(1,6)
+    rolls[dice2 - 1] = rolls[dice2 - 1] + 1
 
+  #find the sum total of the two dice
+    sum_total = dice1 + dice2
+    sum_rolls[sum_total - 2] += 1
+  #print statictics for dice rolls
+  dice = 1
+  for count in rolls:
+    print(dice, ":", count)
+    dice = dice + 1
+
+  print("two dice")
+  for i in range(2, 13):
+    print(i, ":", sum_rolls[i - 2])
 
 if __name__ == '__main__':
   main()
--- WordCount.py
+++ WordCount.py
@@ -5,9 +5,21 @@
 
 def main():
   textFile = open("gettysberg.txt", 'r')
-  
+  lineCount = 0
+  wordCount = 0
+  letterCount = 0
   for line in textFile:
-    print(line)
+    lineCount += 1
+    words = line.split()
+    wordCount += len(words)
+    for char in line:
+      if char.isalpha():
+        letterCount += 1
+    # print(line)
+  
+  print("Lines:", lineCount)
+  print("Words:", wordCount)
+  print("Letters:", letterCount)
   
 
 if __name__ == '__main__':
